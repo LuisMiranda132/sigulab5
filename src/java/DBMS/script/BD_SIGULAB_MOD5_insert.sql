f6ed40b76315564e36c2a886c0c18d9a6302da60
Alejandro <aguillenv93@gmail.com>
2014-01-02 14:46:32 -0430

    Prueba de insercion con visibilidad

--- src/java/DBMS/script/BD_SIGULAB_MOD5_insert.sql
+++ src/java/DBMS/script/BD_SIGULAB_MOD5_insert.sql
@@ -2,8 +2,11 @@
 --********************************* INSERT *************************************
 --******************************************************************************
 
-INSERT INTO USUARIO VALUES ('09-10279', 'Michelle', 'Fernandez', '19505244', 'micafe.go@gmail.com', 'La Castellana', '02122639194', '04120247075', 'Empleado');
+INSERT INTO USUARIO VALUES ('09-10279', 'Michelle', 'Fernandez', '19505244', 'micafe.go@gmail.com', 'La Castellana', '02122639194', '04120247075', 'Empleado',1);
 INSERT INTO EMPLEADO VALUES ('Jefe', '2000', 'jefe', '09-10279');
+
+INSERT INTO USUARIO VALUES ('10-10333', 'Alejandro', 'Guillen', '21516707', 'aguillenv93@gmail.com', 'Macaracuay', '02122570280', '04141731703', 'Empleado',0);
+INSERT INTO EMPLEADO VALUES ('Jefe', '2001', 'jefe', '10-10333');
 
 insert into usuario values ('pperez','Pedro','Perez',235456,'ppedro@usb.ve','Caracas','02123244422','04142334565','empleado');
 insert into usuario values ('robertor','Roberto','Rodriguez',543643,'roberto@gmail.com','Valencia','02544344565','04142355467 ','empleado');
